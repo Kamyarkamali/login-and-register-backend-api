fbfb933b018665cb76dcab5eb44b93ce3bc6214d
kamyarkamali <kamyarkamali021@gmail.com>
2025-11-12 05:25:24 +0330

    create token for route login and register

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,5 +1,9 @@
 const { usserModel } = require("../model/model.user");
-const { hashPassword } = require("../utils/auth.util");
+const {
+  hashPassword,
+  compatePassword,
+  singToken,
+} = require("../utils/auth.util");
 
 async function authRegister(req, res, next) {
   try {
@@ -15,8 +19,24 @@ async function authRegister(req, res, next) {
   }
 }
 
-function login(req, res, next) {
-  res.send("login");
+async function login(req, res, next) {
+  const { email, password } = req.body;
+  const user = await usserModel.findOne({ email });
+
+  if (!user)
+    throw {
+      status: 400,
+      message: "user not found",
+    };
+
+  if (compatePassword(password, user.password)) {
+    const token = singToken({ id: user._id, email: user.email });
+    res.send({ token, message: "register successfully" });
+  } else
+    throw {
+      status: 401,
+      message: "email or password incorret ",
+    };
 }
 
 module.exports = {
--- utils/auth.util.js
+++ utils/auth.util.js
@@ -1,4 +1,5 @@
 const { genSaltSync, hashSync, compareSync } = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 function hashPassword(password) {
   const salt = genSaltSync(11);
@@ -9,7 +10,18 @@ function compatePassword(password, hashed) {
   return compareSync(password, hashed);
 }
 
+// create token
+const singToken = (payload) => {
+  return jwt.sign(payload, process.env.secret);
+};
+
+const verifyToken = (token) => {
+  return jwt.verify(token, process.env.secret);
+};
+
 module.exports = {
   hashPassword,
   compatePassword,
+  singToken,
+  verifyToken,
 };
